61eec1bcfb97b53c10034f77be6b0d5f483e6a71
Mateusnasciment <mateus.nasciment@hotmail.com>
2022-10-28 00:46:23 +0000

    welcome

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text }from 'react-native';
+import { Button }from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <view style={{ ///div
+      paddingInline: 40,
+      paddingTop: 300,
+      textAlign: 'center',
+      backgroundColor: '#201d1d'
+    }}>
+      <text style={{  
+        fontSize: 20,
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        panding: 30
+      }}>
+        Nome do Evento
+        <Button style=
+        {{
+          justifyContent: "space-between",
+          color: '#eb0808'
+          // borderRadius: 10,10,20,10;
+        }}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        title="Login"
+        enabled
+        onPress={() => Alert.alert('Cannot press this one')}
+      />
+      </text>
+
+        <br />
+      
+      <text style={{
+        flex: 1,
+        backgroundColor: 'blue'
+      }}>
+        Mateus
+      </text>
+    </view>
+  )
+}
